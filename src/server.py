ac4bc3770db899da041a5d41c12c3d1366bea2e9
Ray <ray040123@gmail.com>
2011-11-30 02:47:35 +0800

    Most code in this server.py are moved to field_server.py .

--- src/server.py
+++ src/server.py
@@ -5,123 +5,27 @@ import config
 import sys
 import select
 import socket
-import xmlrpclib
 from random import randint
 from field import Field
 from snake import *
 from baseobj import *
+from field_server import FieldServer
 
 X, Y = 0, 1
 fSize = config.field_size1 # the field size
 
-waited = 0
-# to store the name of dead players, if that client send quit, remove the name 
-# from here
-ghosts = set()
-
-
-def startServer():
-	# return a socket obj as server
-	server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	port = config.server_port
-	# try:
-	server.bind(('', port))
-	# except socket.error as e:
-	# 	print e
-	# 	port += 1
-	# 	if port - config.server_port > 10:
-	# 		raise
-	server.listen(config.server_backlog)
-	return server
-
-class IDGener:
-	""" To generate id"""
-	def __init__(self):
-		self._ids = set()
-	def gen(self):
-		while 1:
-			r = randint(100000, 999999)
-			if r not in self._ids:
-				self._ids.add(r)
-				return r
-
-msg_success = "{'cmd':'success'}"
-def parseCommand(field, data, client):
-	global clients, waited
-	msg = eval(data, {'__buildin__':None}, {})
-	cmd = msg['cmd']
-	if cmd == 'add':
-		id = id_gener.gen()
-		clients[id] = [None, client]
-		client.send("{'cmd':'success', 'id':%d}"%(id))
-		print 'client #%s added into this server' % (id)
-
-	if cmd == 'join':
-		# generate an new id, bound it to the client
-		id = msg['id']
-		name = msg['name']
-		clients[id] = [name, client]
-		initpos, direction =  field.newInitPos()
-		snake = BaseSnake(name, initpos, direction, field)
-		field.register(snake)
-		client.send(msg_success)
-		print 'player %s joined the game' % (name)
-	elif cmd == 'quit':
-		# the player quit the game, but watching
-		id = msg['id']
-		name = clients[id][0]
-		clients[id][0] = None
-		if name in ghosts:
-			ghosts.remove(name)
-		client.send(msg_success)
-		print 'player %s quited the game' % (name)
-	elif cmd == 'leave':
-		# the client want to disconnect
-		id = msg['id']
-		del clients[id]
-		client.send(msg_success)
-		print 'client #%s leave this server' % (id)
-	elif cmd == 'sync':
-		id = msg['id']
-		new_msg = {}
-		if clients[id][0] in ghosts:
-			new_msg['youlost'] = 1
-		new_msg['cmd'] = 'sync_info'
-		new_msg['info'] = field.getSyncInfo()
-		new_msg = str(new_msg)
-		print 'sync len', len(new_msg)
-		client.send(new_msg)
-		# print 'sync at round %d' % (field.round)
-	elif cmd == 'response':
-		round = msg['round']
-		if round != field.round:
-			# something wrong
-			client.send("{'cmd': 'fail', 'round':%d, 'reason':'round not match'}"%(field.round))
-		else:
-			id = msg['id']
-			direction = msg['direction']
-			name = clients[id][0]
-			field.acceptCommand(name, direction)
-			client.send(msg_success)
-		
-
 def info():
-	print 'clients', clients
+	print 'clients', server.clients
 	print 'field.snakes', field.snakes
-	print 'waited', waited
-	print 'ghosts', ghosts
+
 def main():
-	global clients, id_gener, reseted, field, ghosts
-	# clients = {id:(name, client), ...}
-	clients = {}
-	id_gener = IDGener()
+	global field, server
 	field = Field(fSize)
-	reseted = True
-	server = startServer()
+	server = FieldServer(("", config.server_port), field)
+
 	splash = '-'*80 + '\nWelcome to SnakeAIC server!\n' + '-'*80
 	print splash
-	print 'name: ', server.getsockname()
-	input = [server, sys.stdin]
+	print 'name: ', server.socket.getsockname()
 
 	frame_rate = config.frame_rate
 
@@ -134,62 +38,33 @@ def main():
 	all_responsed = False
 	frame = 0
 	while not quit:
-		time_cost = 0
-		all_responsed = False
-		while time_cost < time_max:
-		# while (not quit) and (not all_responsed):
-			tm.tick()
-			input_ready, output_ready, exceptready = select.select(input, [], [], float(time_max - time_cost)/1000)
-			# input_ready, output_ready, exceptready = select.select(input, [], [], 1)
-			# print 'input_ready',input_ready
-			for s in input_ready:
-				if s == server:
-					client, address = server.accept()
-					input.append(client)
-				elif s == sys.stdin:
-					# recieve command from stdin, commands can be:
-					#   start #   stop #   restart #   quit
-					command = sys.stdin.readline().rstrip()
-					if command in ['q', 'quit']:
-						quit = True
-					if command in ['r', 'restart']:
-						print 'reset'
-						clients = {}
-						field.reset()
-					if command in ['l', 'list']:
-						info()
-				else:
-					# handle a client socket
-					data = s.recv(config.data_maxsize)
-					if data:
-						# recieved data, begin handle it
-						try:
-							# print 'recieved: ', data
-							parseCommand(field, data, s)
-						except:
-							print 'Warning: this is not a valid message\n\t%s'%data
-							raise
-					else:
-						# maybe connection lost
-						s.close()
-						input.remove(s)
-			# refresh all_responsed
-			all_responsed = True
-			for name, client in clients.itervalues():
-				if name != None and (name not in field._commands):
-					all_responsed = False
-					break
-			time_cost += tm.tick()
+		# all_responsed = False
+		server.serve(time_max)
+		# recieve command from stdin, commands can be:
+		#   start #   stop #   restart #   quit
+		inputready = select.select([sys.stdin], [], [], 0.010)[0]
+		if inputready:
+			command = sys.stdin.readline().rstrip()
+			if command in ['q', 'quit']:
+				quit = True
+			if command in ['r', 'restart']:
+				print 'reset'
+				clients = {}
+				field.reset()
+			if command in ['l', 'list']:
+				info()
+		# refresh all_responsed
+		# all_responsed = True
+		# for name, client in clients.itervalues():
+		# 	if name != None and (name not in field._commands):
+		# 		all_responsed = False
+		# 		break
 
 		# loop the game
 		tm.tick()
 		if field.started:
-			new_ghosts = field.loop()
-			names = [x[0] for x in clients.itervalues()]
-			for g in new_ghosts:
-				if g in names:
-					ghosts.add(g)
-			
+			field.loop()
+
 		time_max = max(1000/frame_rate - tm.tick(), 50)
 
 		tm_fps.tick(frame_rate)
@@ -197,7 +72,7 @@ def main():
 		# if frame % 50 == 0:
 		# 	print 'FPS:',tm_fps.get_fps()
 	print 'Bye'
-		
+
 if __name__ == '__main__':
 	# cProfile.run('main()')
 	main()
